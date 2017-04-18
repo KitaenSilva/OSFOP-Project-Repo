75dd5cdf5ac141b26a6e17997caf843dc2f4bea2
Jonathan de jong <jonathandejong02@gmail.com>
2017-04-18 21:12:41 +0200

    fixes

--- Scripts/misc/getusername.rb
+++ Scripts/misc/getusername.rb
@@ -3,6 +3,10 @@ def getusername
   size = "128"
 
   Win32API.new("Secur32.dll", "GetUserNameEx", ["I", "P", "P"], "I").call(3, name, size)
-
-  return name.unpack("A*")[0].split(" ")[0].capitalize
+  if name.unpack("A*")[0] == ""
+    Win32API.new("Advapi32.dll", "GetUserName", [ "P", "P"], "I").call(name, size)
+    return name.unpack("A*")[0].split(" ")[0].capitalize
+   else
+    return name.unpack("A*")[0].split(" ")[0].capitalize
+  end
 end
